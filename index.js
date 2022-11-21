9250060716dba476ec48f6830c17813a0fd377e1
Jessica Vaughn <jessica.vaughn619@gmail.com>
2022-11-21 14:24:44 -0700

    Solution

--- index.js
+++ index.js
@@ -4,9 +4,33 @@ const events = "wedding";
 function writeCards(names, events) {
     let messages = [];
     for (let i = 0; i < names.length; i++) {
-        messages.push(`Thank you, ${names[i]} for the wonderful ${events} gift!`);
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+        debugger;
     }
     return messages;
 };
 
 writeCards(names, events);
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    let i = 0;
+    while (i < gifts.length) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        i++;
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+function countDown(countdown) {
+    while (countdown >= 0) {
+        console.log(countdown);
+        countdown -= 1;
+        ;
+    }
+}
+
+countDown(10);
