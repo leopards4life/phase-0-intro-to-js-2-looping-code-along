3bd59c688e5eb29e8abfddfe23d5c4b18acc7966
Jessica Vaughn <jessica.vaughn619@gmail.com>
2022-11-21 14:04:35 -0700

    Added thank you card function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const names = ["Jessica", "Steve", "Holly"]; 
+const events = "wedding";
+
+function writeCards(names, events) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]} for the wonderful ${events} gift!`);
+    }
+    return messages;
+};
+
+writeCards(names, events);
